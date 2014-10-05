4436de1894133b695761e5707044b7608ec758ee
Michael Tolly <miketolly@gmail.com>
2014-10-05 00:20:53 -0500

    docs/format

--- src/Drums.hs
+++ src/Drums.hs
@@ -31,6 +31,7 @@ data Game = Game
   , _window   :: Rational
   } deriving (Eq, Ord, Show)
 
+-- | @cut ord k@ removes any key @j@ for which @compare j k == ord@.
 cut :: (Ord k) => Ordering -> k -> Map.Map k v -> Map.Map k v
 cut EQ k m = Map.delete k m
 cut LT k m = case Map.splitLookup k m of
@@ -38,6 +39,10 @@ cut LT k m = case Map.splitLookup k m of
 cut GT k m = case Map.splitLookup k m of
   (lt, eq, _) -> maybe id (Map.insert k) eq lt
 
+-- | Updates the game state for when a pad has been hit by the player.
+-- We look for an unplayed note of that pad to play; if one exists, the combo
+-- and score go up and the note is marked played. Otherwise, an overhit is
+-- logged, and the combo is reset to 0.
 hit :: Pad -> Game -> Game
 hit pad g = let
   hitWindow
@@ -51,10 +56,10 @@ hit pad g = let
     Unplayed p | p == pad -> Played p
     _                     -> e
   multiplier =
-    if _combo g < 10 then 1
+    if      _combo g < 10 then 1
     else if _combo g < 20 then 2
     else if _combo g < 30 then 3
-    else 4
+    else                       4
   in case targets' of
     [] -> g
       { _events = Map.alter addOverhit (_position g) $ _events g
@@ -66,6 +71,9 @@ hit pad g = let
       , _score  = _score g + 100 * multiplier
       }
 
+-- | Moves the game state forward in time.
+-- We look for unplayed notes which can't be played anymore, and if there are
+-- more than zero, the combo is reset to 0.
 moveForward :: Rational -> Game -> Game
 moveForward t g = let
   startMissed = _position g - _window g -- inclusive
